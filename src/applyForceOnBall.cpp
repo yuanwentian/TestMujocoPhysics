648cbd203fa96328f82e55b94cd738213a2e2e13
Yuanwen Tian <yuanwentian@hust.edu.cn>
2018-08-18 00:10:18 -0700

    Aug. 18, 2018

--- src/applyForceOnBall.cpp
+++ src/applyForceOnBall.cpp
@@ -4,61 +4,58 @@
 //  Copyright (C) 2017 Roboti LLC  //
 //---------------------------------//
 
-
-#include "mujoco.h"
-#include "glfw3.h"
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
+#include "math.h"
 #include <iostream>
 
+#include "mujoco.h"
+#include "glfw3.h"
 
 // MuJoCo data structures
-mjModel* m = NULL;                  // MuJoCo model
-mjData* d = NULL;                   // MuJoCo data
-mjvCamera cam;                      // abstract camera
-mjvOption opt;                      // visualization options
-mjvScene scn;                       // abstract scene
-mjrContext con;                     // custom GPU context
+mjModel *m = NULL; // MuJoCo model
+mjData *d = NULL;  // MuJoCo data
+mjvCamera cam;     // abstract camera
+mjvOption opt;     // visualization options
+mjvScene scn;      // abstract scene
+mjrContext con;    // custom GPU context
 
 // mouse interaction
 bool button_left = false;
 bool button_middle = false;
-bool button_right =  false;
+bool button_right = false;
 double lastx = 0;
 double lasty = 0;
 
-
 // keyboard callback
-void keyboard(GLFWwindow* window, int key, int scancode, int act, int mods)
+void keyboard(GLFWwindow *window, int key, int scancode, int act, int mods)
 {
     // backspace: reset simulation
-    if( act==GLFW_PRESS && key==GLFW_KEY_BACKSPACE )
+    if (act == GLFW_PRESS && key == GLFW_KEY_BACKSPACE)
     {
         mj_resetData(m, d);
         mj_forward(m, d);
     }
 }
 
-
 // mouse button callback
-void mouse_button(GLFWwindow* window, int button, int act, int mods)
+void mouse_button(GLFWwindow *window, int button, int act, int mods)
 {
     // update button state
-    button_left =   (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT)==GLFW_PRESS);
-    button_middle = (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_MIDDLE)==GLFW_PRESS);
-    button_right =  (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT)==GLFW_PRESS);
+    button_left = (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS);
+    button_middle = (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_MIDDLE) == GLFW_PRESS);
+    button_right = (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS);
 
     // update mouse position
     glfwGetCursorPos(window, &lastx, &lasty);
 }
 
-
 // mouse move callback
-void mouse_move(GLFWwindow* window, double xpos, double ypos)
+void mouse_move(GLFWwindow *window, double xpos, double ypos)
 {
     // no buttons down: nothing to do
-    if( !button_left && !button_middle && !button_right )
+    if (!button_left && !button_middle && !button_right)
         return;
 
     // compute mouse displacement, save
@@ -72,44 +69,50 @@ void mouse_move(GLFWwindow* window, double xpos, double ypos)
     glfwGetWindowSize(window, &width, &height);
 
     // get shift key state
-    bool mod_shift = (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT)==GLFW_PRESS ||
-                      glfwGetKey(window, GLFW_KEY_RIGHT_SHIFT)==GLFW_PRESS);
+    bool mod_shift = (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS ||
+                      glfwGetKey(window, GLFW_KEY_RIGHT_SHIFT) == GLFW_PRESS);
 
     // determine action based on mouse button
     mjtMouse action;
-    if( button_right )
+    if (button_right)
         action = mod_shift ? mjMOUSE_MOVE_H : mjMOUSE_MOVE_V;
-    else if( button_left )
+    else if (button_left)
         action = mod_shift ? mjMOUSE_ROTATE_H : mjMOUSE_ROTATE_V;
     else
         action = mjMOUSE_ZOOM;
 
     // move camera
-    mjv_moveCamera(m, action, dx/height, dy/height, &scn, &cam);
+    mjv_moveCamera(m, action, dx / height, dy / height, &scn, &cam);
 }
-
 
 // scroll callback
-void scroll(GLFWwindow* window, double xoffset, double yoffset)
+void scroll(GLFWwindow *window, double xoffset, double yoffset)
 {
     // emulate vertical mouse motion = 5% of window height
-    mjv_moveCamera(m, mjMOUSE_ZOOM, 0, -0.05*yoffset, &scn, &cam);
+    mjv_moveCamera(m, mjMOUSE_ZOOM, 0, -0.05 * yoffset, &scn, &cam);
 }
 
-// apply force on the ball by set d -> ctrl
-void applyForceOnModel(const mjModel* m, mjData* d)
+/////TRIAL1: apply force on the ball by set d -> ctrl
+//void applyForceOnModel(const mjModel* m, mjData* d)
+//{
+//    if (d -> time > 5 && d -> time < 8)
+//        d -> ctrl[0] = {-4.0};
+//if (d->time >= 8)
+//    d->ctrl[0] = {4.0};
+//}
+
+/////TRIAL2: using controller
+void mycontroller(const mjModel* m, mjData* d)
 {
-    if (d -> time > 20 && d -> time < 30)
-        d -> ctrl[1] = {-40.0};
-    if (d -> time >=30)
-        d -> ctrl[1] = {40.0};
+   d->ctrl[0] = std::cos(200 * d->time / 10.) * 0.01;
+   d->ctrl[1] = std::sin(200 * d->time / 10.) * 0.01;
 }
 
-// main function
-int main(int argc, const char** argv)
+//main functon
+int main(int argc, const char **argv)
 {
     // check command-line arguments
-    if( argc!=2 )
+    if (argc != 2)
     {
         printf(" USAGE:  basic modelfile\n");
         return 0;
@@ -117,25 +120,25 @@ int main(int argc, const char** argv)
 
     // activate software
     mj_activate("mjkey.txt");
-
     // load and compile model
     char error[1000] = "Could not load binary model";
-    if( strlen(argv[1])>4 && !strcmp(argv[1]+strlen(argv[1])-4, ".mjb") )
-        m = mj_loadModel(argv[1], 0);
+    if (strlen(argv[1]) > 4 && !strcmp(argv[1] + strlen(argv[1]) - 4, ".mjb"))
+       { m = mj_loadModel(argv[1], 0);
+        std::cout<<argv[1][0]<<std::endl;}
     else
         m = mj_loadXML(argv[1], 0, error, 1000);
-    if( !m )
+    if (!m)
         mju_error_s("Load model error: %s", error);
 
     // make data
     d = mj_makeData(m);
 
     // init GLFW
-    if( !glfwInit() )
+    if (!glfwInit())
         mju_error("Could not initialize GLFW");
 
     // create window, make OpenGL context current, request v-sync
-    GLFWwindow* window = glfwCreateWindow(1200, 900, "Demo", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(1200, 900, "Demo", NULL, NULL);
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
@@ -143,8 +146,8 @@ int main(int argc, const char** argv)
     mjv_defaultCamera(&cam);
     mjv_defaultOption(&opt);
     mjr_defaultContext(&con);
-    mjv_makeScene(&scn, 1000);                   // space for 1000 objects
-    mjr_makeContext(m, &con, mjFONTSCALE_100);   // model-specific context
+    mjv_makeScene(&scn, 1000);                 // space for 1000 objects
+    mjr_makeContext(m, &con, mjFONTSCALE_100); // model-specific context
 
     // install GLFW mouse and keyboard callbacks
     glfwSetKeyCallback(window, keyboard);
@@ -157,23 +160,34 @@ int main(int argc, const char** argv)
     std::cout << m->nu << std::endl;
     std::cout << d->ctrl << std::endl;
 
+    
     // run main loop, target real-time simulation and 60 fps rendering
-    while( !glfwWindowShouldClose(window) )
+    while (!glfwWindowShouldClose(window))
     {
         // advance interactive simulation for 1/60 sec
         //  Assuming MuJoCo can simulate faster than real-time, which it usually can,
         //  this loop will finish on time for the next frame to be rendered at 60 fps.
         //  Otherwise add a cpu timer and exit this loop when it is time to render.
         mjtNum simstart = d->time;
-        while( (d->time - simstart < 1.0/60.0) && (d->time < 50) )
+/*         while ((d->time - simstart < 1.0 / 60.0) && (d->time < 11))
         {
             mj_step1(m, d);
             applyForceOnModel(m, d);
-            std::cout << m -> nu << "  " << d -> ctrl[0] << std::endl;
-            std::cout << d -> time << std::endl;
+            std::cout << m->nu << "  " << d->ctrl[0] << std::endl;
+            std::cout << d->time << std::endl;
             mj_step2(m, d);
-        }
-
+        } */
+ 
+         while ((d->time - simstart < 1.0 / 200.0) && (d->time < 11))
+        {
+            mjcb_control = mycontroller;
+            mj_step(m, d);
+      
+        /*    std::cout << m->nu << "  " << d->ctrl[0] << std::endl;
+            std::cout << d->time << std::endl; */
+   
+        } 
+ 
 
         // get framebuffer viewport
         mjrRect viewport = {0, 0, 0, 0};
